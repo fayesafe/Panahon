418b12806f5ade6fd395d08e94906fe04e322dc2
Dominic Pfeil <dominic.pfeil@web.de>
2016-06-01 15:53:42 +0200

    put db init into main package

--- main/main.go
+++ main/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"os"
 
+	"Panahon/database"
 	"Panahon/logger"
 	"Panahon/server"
 	"Panahon/station"
+	"github.com/influxdata/influxdb/client/v2"
 )
 
 // main is the Main Function of the Program
 func main() {
 	logger.Init(os.Stdout, os.Stdout, os.Stderr)
 	logger.Info.Println("Logger initialized")
+
+	influxClient, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		logger.Error.Println(err)
+		os.Exit(1)
+	}
+
+	database.Init(influxClient)
+	logger.Info.Println("InfluxDB client initialized")
+
 	go station.TestRoutine()
 	server.StartServer()
 }
